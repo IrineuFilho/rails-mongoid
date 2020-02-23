c200f1bd59d6f98aa26df5078544b5f0e1ec1a14
Irineu Filho <irineutenorio@gmail.com>
2020-02-23 00:09:53 -0300

    - Added association on Company with complains; - Adjust Model test; - Added mongo.yml translation;

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,8 +2,12 @@
 
 class Company
   include Mongoid::Document
+
   field :name, type: String
   field :cnpj, type: String
 
   belongs_to :city, inverse_of: :company
+
+  has_many :complains, inverse_of: :company, dependent: :destroy
+
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -9,9 +9,16 @@ RSpec.describe Company, type: :model do
 
   describe 'list attributes' do
     it_behaves_like 'number attributes on class', described_class, 4
-    it_behaves_like 'include attributes on class', described_class, '_id'
-    it_behaves_like 'include attributes on class', described_class, 'name'
-    it_behaves_like 'include attributes on class', described_class, 'cnpj'
-    it_behaves_like 'include attributes on class', described_class, 'city_id'
+    it { is_expected.to have_field(:_id).of_type(BSON::ObjectId) }
+    it { is_expected.to have_field(:name).of_type(String) }
+    it { is_expected.to have_field(:cnpj).of_type(String) }
+    it { is_expected.to have_field(:city_id).of_type(Object) }
+  end
+
+  describe 'list associations' do
+    it_behaves_like 'number relations on class', described_class, 2
+
+    it { is_expected.to belong_to(:city).with_foreign_key(:city_id).as_inverse_of(:company) }
+    it { is_expected.to have_many(:complains).as_inverse_of(:company).with_dependent(:destroy)}
   end
 end
